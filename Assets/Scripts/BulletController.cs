8aef6d10711829e58c5fa87bc8324782e1c7f241
Jonathon Anderson <anderbubble@gmail.com>
2015-07-24 19:57:56 -0600

    Moving tanks and shooting and stuff

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float speed = 1;
+	public float lifespan = 1;
+
+	private float fired;
+
+	void Start () {
+		this.fired = Time.time;
+		this.GetComponent<Rigidbody2D>().velocity = (this.transform.rotation * Vector2.up * this.speed);
+	}
+	
+	void Update () {
+		if (Time.time >= this.fired + this.lifespan) {
+			this.Explode ();
+		}
+	}
+
+	void Explode () {
+		this.gameObject.SetActive (false);
+		Destroy (this.gameObject);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		this.Explode ();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public string TurnAxis = "Horizontal";
+	public string MoveAxis = "Vertical";
+	public string FireButton = "Fire";
+	public GameObject bullet;
+
+	private Rigidbody2D Rigidbody;
+
+	void Start () {
+		this.Rigidbody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		var moving = MovePlayer();
+		if (moving) {
+			DampenVelocity();
+		}
+		if (Input.GetButtonDown (this.FireButton)) {
+			FireBullet();
+		}
+	}
+
+	bool MovePlayer () {
+		var turn = Input.GetAxis (this.TurnAxis);
+		var move = Input.GetAxis (this.MoveAxis);
+		var rotation = Quaternion.AngleAxis(-45 * turn * Time.deltaTime, Vector3.forward);
+		this.transform.rotation *= rotation;
+		this.transform.position += this.transform.rotation * Vector3.up * move * Time.deltaTime;
+		return (move != 0);
+	}
+
+
+	void DampenVelocity () {
+		this.Rigidbody.velocity = Vector2.zero;
+		this.Rigidbody.angularVelocity = 0;
+	}
+
+	void FireBullet () {
+		Instantiate(this.bullet, this.transform.position + (this.transform.rotation * Vector3.up * .5f), this.transform.rotation);
+	}
+}
