e88881644c2186fedd7253eaa8c630331b182df8
Jonathon Anderson <anderbubble@gmail.com>
2015-07-26 00:01:50 -0600

    Track score and display score UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,7 +7,35 @@ public class GameController : MonoBehaviour {
 
 	public GameObject Player1;
 	public GameObject Player2;
+
+	public Text Player1ScoreUI;
+	public Text Player2ScoreUI;
+
+	private int _Player1Score = 0;
+	private int _Player2Score = 0;
+
+	private int Player1Score {
+		get {
+			return this._Player1Score;
+		}
+
+		set {
+			this._Player1Score = value;
+			this.Player1ScoreUI.text = this._Player1Score.ToString();
+		}
+	}
 	
+	private int Player2Score {
+		get {
+			return this._Player2Score;
+		}
+		
+		set {
+			this._Player2Score = value;
+			this.Player2ScoreUI.text = this._Player2Score.ToString();
+		}
+	}
+
 	private HashSet<GameObject> RespawnPlayers;
 
 	void Start () {
@@ -15,10 +44,12 @@ public class GameController : MonoBehaviour {
 
 	void Update () {
 		if (!this.RespawnPlayers.Contains(this.Player1) && !this.Player1.activeInHierarchy) {
+			this.Player2Score += 1;
 			this.RespawnPlayers.Add (this.Player1);
 			StartCoroutine (this.Respawn(this.Player1));
 		}
 		if (!this.RespawnPlayers.Contains (this.Player2) && !this.Player2.activeInHierarchy) {
+			this.Player1Score += 1;
 			this.RespawnPlayers.Add (this.Player2);
 			StartCoroutine (this.Respawn(this.Player2));
 		}
