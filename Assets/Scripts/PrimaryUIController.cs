0f1093b7e4fe26a968e7e0921507b543241114f0
Jonathon Anderson <anderbubble@gmail.com>
2015-07-30 22:43:30 -0600

    Add UI, Walls, and Barriers scenes

---
+++ Assets/Scripts/PrimaryUIController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrimaryUIController : MonoBehaviour {
+	void Start () {
+		Application.LoadLevelAdditive ("Tanks");
+		Application.LoadLevelAdditive ("TankWalls");
+		Application.LoadLevelAdditive ("TankBarriers");
+	}
+}
