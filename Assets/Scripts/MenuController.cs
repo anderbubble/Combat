1b1b12a6748afe475ce558130a2d8338f83924a9
Jonathon Anderson <anderbubble@gmail.com>
2015-08-01 13:40:05 -0600

    Move scene configuration to a GameController
    Moving game configuration to a game controller allows the
    menu to be destroyed during the Tank scene load. This is
    important because it prevents the Camera and EventSystem
    in each scene from conflicting with each other.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,26 +5,10 @@ using System.Collections;
 public class MenuController : MonoBehaviour {
 
 	public Toggle BouncingBullets;
-	public BulletController BouncyBulletTemplate;
+	public GameController GameController;
 
 	public void LoadTanks () {
-		Application.LoadLevelAdditive("Tanks");
-		if (this.BouncingBullets.isOn) {
-			StartCoroutine (this.ConfigureBouncingBullets());
-		} else {
-			Destroy (this.gameObject);
-		}
-	}
-
-	public IEnumerator ConfigureBouncingBullets () {
-		GameObject [] players = GameObject.FindGameObjectsWithTag("Player");
-		while (players.Length < 1) {
-			yield return new WaitForEndOfFrame();
-			players = GameObject.FindGameObjectsWithTag("Player");
-		}
-		foreach (var Player in players) {
-			Player.GetComponent<TankController>().BulletTemplate = this.BouncyBulletTemplate;
-		}
-		Destroy(this.gameObject);
+		this.GameController.ConfigureTanks(BouncingBullets: this.BouncingBullets.isOn);
+		Application.LoadLevel("Tanks");
 	}
 }
